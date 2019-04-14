3fb37f04f7635e2994a8eb57e7ae69d34118429a
Fendi Tri Cahyono <fenditricahyono@gmail.com>
2019-04-15 06:47:09 +0700

    model

--- quickstart/models.py
+++ quickstart/models.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
+# from pygments.lexers import get_all_lexers
+# from pygments.styles import get_all_styles
 
 JURUSAN_CHOICES = (
   ('tif','Teknik Informatika'),
