21d9c91dfe0fe88dddfd4cd976e579e5f93c8783
Fendi Tri Cahyono <fenditricahyono@gmail.com>
2019-05-06 06:39:40 +0700

    install ulang

--- restfull/settings.py
+++ restfull/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'restfull.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql', 
