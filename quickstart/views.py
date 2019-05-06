593785c5d7a25a3938eb4f2a7dbfe850a968544b
Fendi Tri Cahyono <fenditricahyono@gmail.com>
2019-05-06 07:34:09 +0700

    add token

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,6 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import viewsets
+from quickstart.serializers import (MahasiswaSerializer,JurusanSerializer,MatakuliahSerializer,NilaiSerializer)
+from quickstart.models import (Mahasiswa,Jurusan,Matakuliah,Nilai)
+
+from rest_framework.authentication import (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+from rest_framework.permissions import IsAuthenticated
 
 class PenjumlahanView(APIView):
   def post(self, request):
@@ -11,12 +17,17 @@ class PenjumlahanView(APIView):
       "hasil": hasil
     }
     return Response(data,status=200)
-
+import sys
 class PertamaView(APIView):
-
+  authentication_classes = (TokenAuthentication,
+                              BasicAuthentication)
+  permission_classes = (IsAuthenticated,)
+  
   def get(self, request):
     data = {
-      "data":"Halo"
+      "data":"Halo",
+      'user': str(request.user),  # `django.contrib.auth.User` instance.
+      'auth': str(request.auth),  # None
     }
     return Response(data,status=200)
   
@@ -63,60 +74,60 @@ class DuaView(APIView):
     }
     return Response(data,status=200)
 
-from rest_framework import viewsets
-from quickstart.serializers import (MahasiswaSerializer,JurusanSerializer,MatakuliahSerializer,NilaiSerializer)
-from quickstart.models import (Mahasiswa,Jurusan,Matakuliah,Nilai)
-
 class MahasiswaViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Mahasiswa.objects.all()
-    serializer_class = MahasiswaSerializer
+  authentication_classes = (TokenAuthentication,
+                            SessionAuthentication,
+                            BasicAuthentication)
+  permission_classes = (IsAuthenticated,)
+  """
+  API endpoint that allows groups to be viewed or edited.
+  """
+  queryset = Mahasiswa.objects.all()
+  serializer_class = MahasiswaSerializer
 
-    def get_nilai(self,request):
-      return Response({"asdf":"asdf"},200)
+  def get_nilai(self,request):
+    return Response({"asdf":"asdf"},200)
 
-    '''
-      {
-        "nim":"",
-        "matakuliah_id":"",
-        "nilai":""
-      }
-    '''
-    def post_nilai(self,request):
-      mahasiswa = Mahasiswa.objects.filter(nim=request.data.get("nim")).first()
-      matakuliah = Matakuliah.objects.get(pk=request.data.get("matakuliah_id"))
+  '''
+    {
+      "nim":"",
+      "matakuliah_id":"",
+      "nilai":""
+    }
+  '''
+  def post_nilai(self,request):
+    mahasiswa = Mahasiswa.objects.filter(nim=request.data.get("nim")).first()
+    matakuliah = Matakuliah.objects.get(pk=request.data.get("matakuliah_id"))
 
-      nilai = Nilai.objects.filter(mahasiswa=mahasiswa,matakuliah=matakuliah).first()
-      if nilai is None:
-        nilai = Nilai()
-      nilai.mahasiswa = mahasiswa
-      nilai.matakuliah = matakuliah
-      nilai.nilai = request.data.get("nilai")
-      action = nilai.save()
+    nilai = Nilai.objects.filter(mahasiswa=mahasiswa,matakuliah=matakuliah).first()
+    if nilai is None:
+      nilai = Nilai()
+    nilai.mahasiswa = mahasiswa
+    nilai.matakuliah = matakuliah
+    nilai.nilai = request.data.get("nilai")
+    action = nilai.save()
 
-      print(action)
-      
-      return Response({"message":"ini method post nilai","status":True},200)
+    print(action)
+    
+    return Response({"message":"ini method post nilai","status":True},200)
 
 class JurusanViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Jurusan.objects.all()
-    serializer_class = JurusanSerializer
+  """
+  API endpoint that allows groups to be viewed or edited.
+  """
+  queryset = Jurusan.objects.all()
+  serializer_class = JurusanSerializer
 
 class MatakuliahViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Matakuliah.objects.all()
-    serializer_class = MatakuliahSerializer
+  """
+  API endpoint that allows groups to be viewed or edited.
+  """
+  queryset = Matakuliah.objects.all()
+  serializer_class = MatakuliahSerializer
 
 class NilaiViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Nilai.objects.all()
-    serializer_class = NilaiSerializer
+  """
+  API endpoint that allows groups to be viewed or edited.
+  """
+  queryset = Nilai.objects.all()
+  serializer_class = NilaiSerializer
--- restfull/settings.py
+++ restfull/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     # mayor app
     'restfull',
     'quickstart',
@@ -135,5 +136,10 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
